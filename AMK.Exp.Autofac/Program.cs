386ca4d813ddefab254c44281b291ae63a8c10e0
Manikanta Annavarapu <manikanta.annavarapu95@gmail.com>
2023-01-25 10:27:20 +0530

    project init

---
+++ AMK.Exp.Autofac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AMK.Exp.Autofac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
