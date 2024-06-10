b5e4a5f5bdd64cbb70a724bf6a667d1ad2b30517
Manikanta Annavarapu <manikanta.annavarapu95@gmail.com>
2024-06-10 15:30:25 +0530

    updated polymorphism code

--- VirtualOverrideAndNewPolymorphism/OverrideState.cs
+++ VirtualOverrideAndNewPolymorphism/OverrideState.cs
@@ -21,22 +21,29 @@ namespace VirtualOverrideAndNewPolymorphism
         class C : B
         {
             public override void Test() { Console.WriteLine("C::Test()"); }
+            public void Test2() { Console.WriteLine("C::Test2()"); }
         }
 
         public void Run()
         {
             A a = new A();
-            a.Test();
+            a.Test(); // A::Test()
             B b = new B();
-            b.Test();
+            b.Test(); // B::Test()
             C c = new C();
-            c.Test();
+            c.Test(); // C::Test()
 
             a = new B();
-            a.Test();
+            a.Test(); // B::Test()
 
             b = new C();
-            b.Test(); // 
+            b.Test(); // C::Test()
+
+            a = new C();
+            a.Test(); // C::Test()
+
+            a = new C();
+            //a.Test2(); // Compile error class does not contain a definition of Test2  and has not extensible method of Test2 which takes A as a first argument.
 
         }
     }
--- VirtualOverrideAndNewPolymorphism/WarningState.cs
+++ VirtualOverrideAndNewPolymorphism/WarningState.cs
@@ -10,34 +10,61 @@ namespace VirtualOverrideAndNewPolymorphism
     {
         class A
         {
-            public void Test() { Console.WriteLine("A::Test()"); }
+            public virtual void Test() { Console.WriteLine("A::Test()"); }
         }
 
         class B : A
         {
-            public void Test() { Console.WriteLine("B::Test()"); }
+            // A warning will be shown here
+            // 'WarningState.B.Test()' hides inherited member 'WarningState.A.Test()'. To make the current method override that implementation, add the override keyword. Otherwise add the new keyword.
+            // irrespective of whether virtual keyword is used in the base class (or current class) or not.
+            public virtual void Test() { Console.WriteLine("B::Test()"); }
         }
 
         class C : B
         {
-            public void Test() { Console.WriteLine("C::Test()"); }
+            public virtual void Test() { Console.WriteLine("C::Test()"); }
         }
+
+        class B2MethodHiding : A
+        {
+            
+            public new void Test() { Console.WriteLine("B2MethodHiding::Test()"); }
+        }
+
+        class C2 : B2MethodHiding
+        {
+            //'WarningState.C2.Test()' : cannot override inherited member 'WarningState.B2.Test()' because it is not marked "virtual", "abstract", or "override"
+            //public override void Test() { Console.WriteLine("C2::Test()"); }
+        }
+
+        class C3 : A
+        {
+            public override void Test() { Console.WriteLine("C3::Test()"); }
+        }
+
 
         public void Run()
         {
             A a = new A();
-            a.Test();
+            a.Test(); // A::Test()
             B b = new B();
-            b.Test();
+            b.Test(); // B::Test()
             C c = new C();
-            c.Test();
+            c.Test(); // C::Test()
 
             a = new B();
-            a.Test();
+            a.Test(); // A::Test()
 
             b = new C();
-            b.Test(); // 
+            b.Test(); // B::Test()
 
+
+            B2MethodHiding b2MethodHiding = new B2MethodHiding();
+            b2MethodHiding.Test(); // B2MethodHiding::Test()
+
+            a = new B2MethodHiding();
+            a.Test(); // A::Test()
         }
     }
 }
